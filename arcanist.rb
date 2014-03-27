fcbfbab1eb0698317ea47c767f42cfc7f7696598
Alessio Deiana <alessio.deiana@cern.ch>
2014-03-27 16:24:56 +0100

    Removes outdated sha1

--- arcanist.rb
+++ arcanist.rb
@@ -3,13 +3,12 @@ require 'formula'
 class Arcanist < Formula
   homepage 'http://phabricator.org/'
   url 'https://github.com/facebook/arcanist/tarball/master'
-  sha1 'eb21e42d641a309258a85cba81d28b556dea22ea'
   version '0.1'
 
   depends_on 'libphutil'
 
   head 'https://github.com/facebook/arcanist.git'
-  
+
   def install
     # Get the location of libphutil so you can link arcanist to them
     var = `brew --prefix libphutil`.strip
@@ -21,7 +20,7 @@ class Arcanist < Formula
     cd "externals/includes" do
       File.symlink(var, "libphutil")
     end
-    
+
     # Remove Windows scripts - BOOM!
     rm_rf Dir['bin/*.bat']
 
